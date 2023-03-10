b44e82d602d811cbaf785a78f055e217a2704f4f
Kaustubh Pathak <kaustubhpathak9@gmail.com>
2023-03-11 00:55:27 +0530

    css

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -15,7 +15,7 @@ import icon from '../../assets/icon.png'
 
 //importing component
 import { login } from '../../actions/auth'
-
+import LeftsideBar from '../../components/LeftsideBar/LeftsideBar'
 
 //main function goes here
 const Login = () => {
@@ -37,28 +37,32 @@ const Login = () => {
 
   return (
     <div className='loginPage'>
+      <div className="home-container-1">
+        <LeftsideBar />
+      </div>
+      
+        <img src={icon} alt='logo-icon'></img>
 
-      <img src={icon} alt='logo-icon'></img>
+        <form id='loginform' onSubmit={handleSubmit}>
 
-      <form id='loginform' onSubmit={handleSubmit}>
-        
           <h5 style={{ fontSize: "18px" }}>Email</h5>
           <input type="email" name='email' required id='email' onChange={(e) => { setEmail(e.target.value) }} />
           <br />
-        
-          
-          <h5 style={{ fontSize: "18px" }} id="pass">Password</h5>         
+
+
+          <h5 style={{ fontSize: "18px" }} id="pass">Password</h5>
           <Link to='/forgotpassword' id='forgotpass'>Forgot password?</Link>
-          
+
           <div className="password-container">
-          <input type="password" name="password" required id="password" onChange={(e) => { setPassword(e.target.value) }} /><br /></div>
-        <br />
+            <input type="password" name="password" required id="password" onChange={(e) => { setPassword(e.target.value) }} /><br /></div>
+          <br />
 
-        <button type="submit" name='submit' className='loginButton' > Log in</button>
-      </form>
+          <button type="submit" name='submit' className='loginButton' > Log in</button>
+        </form>
 
 
-      <p>Don't have an account ? </p><Link to='/signup' id="signuplink">Sign up</Link>
+        <p id='sign'>Don't have an account ? </p><Link to='/signup' id="signuplink">Sign up</Link>
+      
       <ToastContainer />
     </div>
   )
