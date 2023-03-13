bee68de1a1dd90364b609e1524fbf746c8fd7b73
Kaustubh Pathak <kaustubhpathak9@gmail.com>
2023-03-13 18:20:59 +0530

    update

--- src/Pages/Payment/Paymentsuccess.jsx
+++ src/Pages/Payment/Paymentsuccess.jsx
@@ -5,14 +5,14 @@ import LeftSidebar from "../../components/LeftsideBar/LeftsideBar"
 import success from "../../assets/PAYMENT-SUCCESS (1).png"
 import Button from 'react-bootstrap/Button'
 const Paymentsuccess = () => {
-  const navigate =useNavigate();
+  const navigate = useNavigate();
   const searchQuery = useSearchParams()[0]
   const reference = searchQuery.get("reference");
-  const gohome =(e)=>{
+  const gohome = (e) => {
     e.preventDefault()
     navigate("/");
   }
-  const ask =(e)=>{
+  const ask = (e) => {
     e.preventDefault()
     navigate("/AskQuestions");
   }
@@ -23,13 +23,13 @@ const Paymentsuccess = () => {
       </div>
       <div className='home-container-2' style={{ display: "block", textAlign: "center" }}>
         <div className="ref">Reference No.  <b>{reference}</b></div><br />
-<div class="row" style={{width:"90%"}}>
-<img src={success} alt="success" style={{ marginLeft: "auto" }} />
-</div>
-        
-        <div class="row" style={{width:"90%"}}>
-          <div class="col-lg-6" style={{textAlign:"center"}}><Button variant='outline-warning' onClick={gohome}>Browse more Questions </Button></div>
-          <div class="col-lg-6" style={{textAlign:"center"}}><Button variant='outline-warning' onClick={ask}>Ask Question</Button></div>
+        <div class="row" style={{ width: "90%" }}>
+          <img src={success} alt="success" style={{ marginLeft: "auto" }} />
+        </div>
+
+        <div class="row" style={{ width: "90%" }}>
+          <div class="col-lg-6" style={{ textAlign: "center" }}><Button variant='outline-warning' onClick={gohome}>Browse more Questions </Button></div>
+          <div class="col-lg-6" style={{ textAlign: "center" }}><Button variant='outline-warning' onClick={ask}>Ask Question</Button></div>
         </div>
       </div>
     </div>
