d7ac172ec370468e51edd1cf6d1c68ec803596c7
Kaustubh Pathak <kaustubhpathak9@gmail.com>
2023-03-27 14:48:21 +0530

    ui update

--- src/Pages/ForgotPassword/Forgotpassword.jsx
+++ src/Pages/ForgotPassword/Forgotpassword.jsx
@@ -8,18 +8,21 @@ import { reset } from '../../actions/auth';
 
 import './Forgotpassword.css'
 import Spinner from 'react-bootstrap/esm/Spinner';
+import Button from 'react-bootstrap/esm/Button';
+import Form from 'react-bootstrap/esm/Form';
+import Card from 'react-bootstrap/esm/Card';
 const Forgotpassword = () => {
-    const [loading,setLoading]=useState(false);
+    const [loading, setLoading] = useState(false);
     const [email, setEmail] = useState('');
     const [pass1, setPass1] = useState('');
     const [pass2, setPass2] = useState('');
-    const navigate=useNavigate();
+    const navigate = useNavigate();
     const dispatch = useDispatch();
     const resetPassword = (e) => {
         e.preventDefault()
         setLoading(true);
-        setTimeout(()=>{setLoading(false)},5000)
-        if (pass1 === pass2) { dispatch(reset({email, pass1},navigate))}
+        setTimeout(() => { setLoading(false) }, 5000)
+        if (pass1 === pass2) { dispatch(reset({ email, pass1 }, navigate)) }
         else {
             toast('new password should be same in both fields !');
         }
@@ -31,27 +34,55 @@ const Forgotpassword = () => {
             <div className="home-container-1 ">
                 <LeftsideBar />
             </div>
-            <div className='home-container-2'id='forgot-form'>
-                <form onSubmit={resetPassword} id='forgotfrom'>
+            <div className='home-container-2' id='forgot-form'>
+
+                <Card style={{ width: '18rem' }}>
+                    <Card.Body>
+                        <Form onSubmit={resetPassword}>
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Label>Email Id</Form.Label>
+                                <Form.Control type="email" placeholder="Enter email" name="email" required onChange={(e) => { setEmail(e.target.value) }} />
+
+                            </Form.Group>
+
+                            <Form.Group className="mb-3" controlId="formBasicPassword">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type="password" placeholder="Password" name="" autoComplete='off' required onChange={(e) => { setPass1(e.target.value) }} />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="formBasicPassword">
+                                <Form.Label>Re-enter Password</Form.Label>
+                                <Form.Control type="password" placeholder="Password" name="" autoComplete='off' required onChange={(e) => { setPass2(e.target.value) }} />
+                            </Form.Group>
+                            <div className="submission" style={{textAlign:"center"}}>
+                                <Button variant="primary" type="submit" disabled={loading} style={{width:"100%"}}>
+                                    {loading ? (<><Spinner animation="border" variant="light" size='sm' style={{ marginRight: "5px" }} /></>) : (<>Reset Password</>)}
+                                </Button>
+                            </div>
+
+                        </Form>
+                    </Card.Body>
+                </Card>
+
+                {/* <form onSubmit={resetPassword} id='forgotfrom' class="">
                     <label htmlFor="ForgotPassword">
-                        Email:<br/>
+                        Email:<br />
                         <input type="email" name="email" id="forgottenemail" required onChange={(e) => { setEmail(e.target.value) }} />&nbsp;&nbsp;&nbsp;
 
 
                     </label><br />
                     <label htmlFor="">
-                        New Password:<br/>
+                        New Password:<br />
                         <input type="password" name="" id="new" required onChange={(e) => { setPass1(e.target.value) }} />
 
                     </label><br /><br />
                     <label htmlFor="">
-                    Re Enter New Password:<br/>
+                        Re Enter New Password:<br />
                         <input type="password" name="" id="reenter" required onChange={(e) => { setPass2(e.target.value) }} />
                     </label><br /><br />
-                    <button type="submit" className='forgotreset' disabled={loading}>{loading?(<><Spinner animation="border" variant="light" size='sm' style={{marginRight:"5px"}}/></>):(<>Reset Password</>)}</button>
-                </form>
+                    <button type="submit" className='forgotreset' disabled={loading}>{loading ? (<><Spinner animation="border" variant="light" size='sm' style={{ marginRight: "5px" }} /></>) : (<>Reset Password</>)}</button>
+                </form> */}
             </div>
-            <ToastContainer/>
+            <ToastContainer />
         </div>
     )
 }
--- src/Pages/VerifyOTP/VerifyOTP.jsx
+++ src/Pages/VerifyOTP/VerifyOTP.jsx
@@ -8,20 +8,23 @@ import { useDispatch, useSelector } from 'react-redux'
 import './VerifyOTP.css'
 import { verifyOTP } from '../../actions/auth'
 import Spinner from 'react-bootstrap/esm/Spinner';
+import Button from 'react-bootstrap/esm/Button';
+import Form from 'react-bootstrap/esm/Form';
+import Card from 'react-bootstrap/esm/Card';
 const VerifyOTP = () => {
-    
-    const [loading,setLoading]=useState(false);
-    const[email,setEmail]=useState('');
+
+    const [loading, setLoading] = useState(false);
+    const [email, setEmail] = useState('');
     const [otp, setOtp] = useState('');
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const handleVerify = (e) => {
         e.preventDefault()
         setLoading(true)
-        setTimeout(()=>{setLoading(false)},5000)
-        dispatch(verifyOTP({email,otp}, navigate));
+        setTimeout(() => { setLoading(false) }, 5000)
+        dispatch(verifyOTP({ email, otp }, navigate));
     }
-    
+
 
     return (
         <div className='verifypage'>
@@ -30,30 +33,56 @@ const VerifyOTP = () => {
             </div>
             <div className="home-container-2" id='verification'>
                 <div className="form-container" id='verifycontent'>
-                    <div className="verify_header" style={{textAlign:'center'}}>
-                    <h4 style={{marginBottom:"20px"}}>Verify Your Account! </h4>
+                    <div className="verify_header" style={{ textAlign: 'center' }}>
+                        <h4 style={{ marginBottom: "20px" }}>Verify Your Account! </h4>
                     </div>
-                    <div className="alert" style={{textAlign:"center"}}>
-                        <p style={{color:"red"}}>Do not Refresh the Page</p>
+                    <div className="alert" style={{ textAlign: "center" }}>
+                        <p style={{ color: "red" }}>Do not Refresh the Page</p>
                     </div>
-                    <form onSubmit={handleVerify} id='verifyform'>
-                        
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Body>
+                            <Form onSubmit={handleVerify}>
+                                <Form.Group className="mb-3" controlId="formBasicEmail">
+                                    <Form.Label>Email Id</Form.Label>
+                                    <Form.Control type="email" placeholder="Paste or Enter Copied email  " name="email" required onChange={(e) => { setEmail(e.target.value) }} style={{fontSize:"12px"}}/>
+
+                                </Form.Group>
+
+                                <Form.Group className="mb-3" controlId="formBasicPassword">
+                                    <Form.Label>OTP</Form.Label><br/>
+                                    <Form.Text className="text-muted">
+                                        OTP is sent to your Email
+                                    </Form.Text><br/><br/>
+                                    <Form.Control type="text" placeholder="Enter OTP" name="" onChange={(e) => { setOtp(e.target.value) }} autoComplete="off" required style={{fontSize:"12px"}}/>
+                                </Form.Group>
+
+                                <div className="submission" style={{ textAlign: "center" }}>
+                                    <Button variant="primary" type="submit" disabled={loading} style={{ width: "100%"}}>
+                                        {loading ? (<><Spinner animation="border" variant="light" size='sm' style={{ marginRight: "5px" }} /></>) : (<>Verify</>)}
+                                    </Button>
+                                </div>
+
+                            </Form>
+                        </Card.Body>
+                    </Card>
+                    {/* <form onSubmit={handleVerify} id='verifyform'>
+
                         <label htmlFor="email">
                             <h6> Your Email </h6>
                             <input type="email" id='fillemail' onChange={(e) => setEmail(e.target.value)} required placeholder='Paste or Enter Copied email  '></input>
                         </label>
-                        <br /><br/>
+                        <br /><br />
                         <label htmlFor="otp">
                             <h6>Enter OTP sent via Email </h6>
-                            <input type="text" onChange={(e) => { setOtp(e.target.value) }} autoComplete="off" id='fillotp'/><br /><br />
-                            <button type="submit" name='submit' disabled={loading} className="verifybutton">{loading?(<><Spinner animation="border" variant="light" size='sm' style={{marginRight:"5px"}}/></>):(<>Verify</>)}</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                            
+                            <input type="text" onChange={(e) => { setOtp(e.target.value) }} autoComplete="off" id='fillotp' /><br /><br />
+                            <button type="submit" name='submit' disabled={loading} className="verifybutton">{loading ? (<><Spinner animation="border" variant="light" size='sm' style={{ marginRight: "5px" }} /></>) : (<>Verify</>)}</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
                         </label>
-                    </form>
-                    
+                    </form> */}
+
                 </div>
             </div>
-            <ToastContainer/>
+            <ToastContainer />
         </div>
     )
 }
