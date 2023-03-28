f8fe054f65710fad1cea84d871ac11ab50e16c39
Kaustubh Pathak <kaustubhpathak9@gmail.com>
2023-03-28 19:44:21 +0530

    ui update

--- src/Pages/UserProfile/ProfileBio.jsx
+++ src/Pages/UserProfile/ProfileBio.jsx
@@ -1,35 +1,45 @@
 import React from 'react'
+import Card from 'react-bootstrap/Card';
 
 const ProfileBio = ({ currentProfile }) => {
     return (
         <div>
-            <div>
-            {
-                currentProfile?.tags.length !==0  ? (
-                    <>
-                        <br />
-                        <h5>Tags Watched</h5>
-                        {currentProfile?.tags.map((tag) => (
-                            <p key={tag}>{tag}</p>
-                        ))}
-                    </>
-                ) : (
-                    <p>0 tags Watched</p>
-                )
-            }
-            </div>
-            <div>
-                {
-                    currentProfile?.about?(
-                        <>
-                            <h5>About</h5>
-                            <p>{currentProfile?.about}</p>
-                        </>
-                    ):(
-                        <p>No Bio Found</p>
-                    )
-                }
-            </div>
+            <br />
+            <Card style={{ width: '100%',boxShadow: "rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset",padding:"20px" }}>
+                <div>
+                    {
+                        currentProfile?.tags.length !== 0 ? (
+                            <>
+                                <br />
+                                <h5>Tags Watched</h5>
+                                {currentProfile?.tags.map((tag) => (
+                                    <p key={tag}>{tag}</p>
+                                ))}
+                            </>
+                        ) : (
+                            <p>0 tags Watched</p>
+                        )
+                    }
+                </div>
+            </Card>
+            <br />
+            <Card style={{ width: '100%',boxShadow: "rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset",padding:"20px" }}>
+                <div>
+                    {
+                        currentProfile?.about ? (
+                            <>
+                                <h5>About</h5>
+                                <p>{currentProfile?.about}</p>
+                            </>
+                        ) : (
+                            <p>No Bio Found</p>
+                        )
+                    }
+                </div>
+            </Card>
+
+
+
         </div>
     )
 }
--- src/Pages/UserProfile/UserProfile.jsx
+++ src/Pages/UserProfile/UserProfile.jsx
@@ -10,8 +10,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBirthdayCake, faPen, faTrash } from "@fortawesome/free-solid-svg-icons";
-
-
+import Card from 'react-bootstrap/Card';
 import EditProfileForm from "./EditProfileForm";
 import ProfileBio from "./ProfileBio";
 import LeftSidebar from "../../components/LeftsideBar/LeftsideBar";
@@ -47,40 +46,44 @@ const UserProfile = () => {
       </div>
       <div className="home-container-2">
         <section id="content">
-          <div className="user-details">
-            <Avatar
-              backgroundColor="#53a2e8"
+          
+          <Card style={{ width: '100%',padding:"20px",boxShadow: "rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset" }}>
+            <div className="user-details">
+              <Avatar
+                backgroundColor="#53a2e8"
 
-              color="#ffff"
-              fontSize="50px"
-              px="4%"
-              py="3%"
-              borderRadius="5px"
-            >
-              {currentProfile?.name.charAt(0).toUpperCase()}
-            </Avatar>
+                color="#ffff"
+                fontSize="50px"
+                px="4%"
+                py="3%"
+                borderRadius="5px"
+              >
+                {currentProfile?.name.charAt(0).toUpperCase()}
+              </Avatar>
 
-            <div className="user-name">
-              <h1>{currentProfile?.name}</h1>
-              <p>
-                <FontAwesomeIcon icon={faBirthdayCake} /> &nbsp; Joined{" "}
-                {moment(currentProfile?.joinedOn).fromNow()}
-              </p>
-              <p>{currentUser?.result._id === id && (
-                <>
+              <div className="user-name">
+                <h1>{currentProfile?.name}</h1>
+                <p>
+                  <FontAwesomeIcon icon={faBirthdayCake} /> &nbsp; Joined{" "}
+                  {moment(currentProfile?.joinedOn).fromNow()}
+                </p>
+                <p>{currentUser?.result._id === id && (
+                  <>
 
 
 
-                  <Button variant="secondary" onClick={handleSubmit}>{loading ? (<><Spinner animation="border" variant="light" size='sm' /></>) : (<>  <FontAwesomeIcon icon={faTrash} /> Delete Account</>)}</Button>{' '}
-                  <Button variant="secondary" onClick={() => setSwitch(true)}> {" "}
-                    <FontAwesomeIcon icon={faPen} /> Edit Account</Button>{' '}
-                </>
+                    <Button variant="secondary" onClick={handleSubmit} className="deletebutton">{loading ? (<><Spinner animation="border" variant="light" size='sm' /></>) : (<>  <FontAwesomeIcon icon={faTrash} /> Delete Account</>)}</Button>{' '}
+                    <Button variant="secondary" onClick={() => setSwitch(true)} className="editaccountbutton"> {" "}
+                      <FontAwesomeIcon icon={faPen} /> Edit Account</Button>{' '}
+                  </>
 
-              )}</p>
+                )}</p>
+              </div>
+
             </div>
+          </Card>
 
-          </div>
-
+          
           <>
             {Switch ? (
               <EditProfileForm
@@ -88,10 +91,11 @@ const UserProfile = () => {
                 setSwitch={setSwitch}
               />
             ) : (
-
               <ProfileBio currentProfile={currentProfile} />
             )}
           </>
+          
+          
         </section>
       </div >
       <ToastContainer />
