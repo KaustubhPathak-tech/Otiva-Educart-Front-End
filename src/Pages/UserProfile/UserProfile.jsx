06dec2d78d49d30e79eb990c6f91615d0e1b581c
Kaustubh Pathak <kaustubhpathak9@gmail.com>
2023-03-28 19:54:04 +0530

    asd

--- src/Pages/UserProfile/UserProfile.jsx
+++ src/Pages/UserProfile/UserProfile.jsx
@@ -47,7 +47,7 @@ const UserProfile = () => {
       <div className="home-container-2">
         <section id="content">
           
-          <Card style={{ width: '100%',padding:"20px",boxShadow: "rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset" }}>
+          <Card style={{ width: '100%',padding:"13px",boxShadow: "rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset" }}>
             <div className="user-details">
               <Avatar
                 backgroundColor="#53a2e8"
