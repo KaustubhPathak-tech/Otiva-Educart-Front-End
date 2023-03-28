285c8f2647b1a15b5a907f702001351bdb6eaf3e
Kaustubh Pathak <kaustubhpathak9@gmail.com>
2023-03-28 09:24:26 +0530

    ui up

--- src/App.js
+++ src/App.js
@@ -40,7 +40,6 @@ import { setCurrentUser } from "./actions/currentUser";
 function App() {
   const [loading, setLoading] = useState(false);
   var payment = useSelector((state) => state.fetch_current_userReducer);
-  console.log(payment);
   var data = payment?.time;
   // var stat = payment?.status;
   var expiry = data + 3600000;
@@ -68,19 +67,18 @@ function App() {
   } else {
     setInterval(refresh, diff);
   }
-  window.screen.orientation.lock('portrait');
+
   const [dsa, setDsa] = useState(false);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchAllquestions());
     dispatch(fetchAllUsers());
   }, [dispatch]);
-
   return (
     <div className="App">
       {loading ? (
         <>
-          <div className="preloader">
+          <div className="preloader portrait">
             <BounceLoader
               className="App"
               color={"#f59607"}
@@ -94,154 +92,162 @@ function App() {
         </>
       ) : (
         <>
-          <BrowserRouter>
-            {/* This is navigation bar */}
+          <div id="landa">
+            
+            <img
+              src="https://storage.googleapis.com/support-forums-api/attachment/thread-54903774-5712120669965295764.jpg"
+              width="100%" height="100%"
+            ></img>
+          </div>
+          <div className="portrait">
+            <BrowserRouter>
+              {/* This is navigation bar */}
 
-            <Popup trigger={dsa} setTrigger={setDsa} onClose={dsa}>
-              <h5 style={{ textAlign: "center" }}>
-                {" "}
-                <blink>
-                  <div style={{ color: "red", textAlign: "center" }}>
-                    <b>ALERT !</b>{" "}
-                  </div>
-                </blink>{" "}
+              <Popup trigger={dsa} setTrigger={setDsa} onClose={dsa}>
+                <h5 style={{ textAlign: "center" }}>
+                  {" "}
+                  <blink>
+                    <div style={{ color: "red", textAlign: "center" }}>
+                      <b>ALERT !</b>{" "}
+                    </div>
+                  </blink>{" "}
+                  <br />
+                  &nbsp;&nbsp;All Payments are in{" "}
+                  <b style={{ color: "red" }}>Test Mode</b>
+                </h5>
                 <br />
-                &nbsp;&nbsp;All Payments are in{" "}
-                <b style={{ color: "red" }}>Test Mode</b>
-              </h5>
-              <br />
-              <h5 style={{ textAlign: "center" }}>Use these Card Details ⬇️</h5>
-              <p style={{ position: "absolute", marginLeft: "16%" }}>
-                Card No : 4111 1111 1111 1111
-              </p>
-              <br />
-              <p style={{ position: "absolute", marginLeft: "16%" }}>
-                Expiry : Any future date
-              </p>
-              <br />
-              <p style={{ position: "absolute", marginLeft: "16%" }}>
-                Card Holder's Name : Any Name
-              </p>
-              <br />
-              <p style={{ position: "absolute", marginLeft: "16%" }}>
-                CVV : Any number
-              </p>
-              <br />
-              <br />
-              <h5 style={{ textAlign: "center" }}>Use these UPI Details ⬇️</h5>
-              <p style={{ position: "absolute", marginLeft: "16%" }}>
-                UPI id: success@razorpay
-              </p>
-              <br />
-            </Popup>
-            <div className="landscape">
-              <img
-                src="https://storage.googleapis.com/support-forums-api/attachment/thread-54903774-5712120669965295764.jpg"
-                width="50"
-              ></img>
-            </div>
-            <Navbar />
-
-            <Routes>
-              {/* This is Home Page Route */}
-              <Route path="/" element={<Home />} />
-
-              {/* This is Login Page Route */}
-              <Route path="/login" element={<Login />}></Route>
-              <Route
-                path="/forgotpassword"
-                element={<Forgotpassword />}
-              ></Route>
-              {/* This is Signup Page Route */}
-              <Route path="/signup" element={<Signup />}></Route>
-              <Route path="/Pricing" element={<Pricing />}></Route>
-              <Route
-                path="/paymentsuccess"
-                element={<Paymentsuccess />}
-              ></Route>
-              <Route path="/verify" element={<VerifyOTP />}></Route>
-              {/* This is User Page Route */}
-              <Route path="/users" element={<User />}></Route>
-              <Route path="/users/:id" element={<UserProfile />}></Route>
-              {/*This is Question Page Route*/}
-              <Route path="/Questions" element={<Questions />}></Route>
-              {/*This is Ask Question Page Route*/}
-              <Route path="/AskQuestions" element={<AskQuestion />}></Route>
-              {/*This is Ask Question Page Route*/}
-              <Route
-                path="/Questions/:id"
-                element={<DisplayQuestion />}
-              ></Route>
-              {/*This is Tags Page Route*/}
-              <Route path="/tags" element={<Tags />}></Route>
-            </Routes>
-
-            <Chat />
-            <div className="footer">
-              <div className="formalities">
-                <span className="privacy">
-                  <a
-                    href="https://www.freeprivacypolicy.com/live/38db0dd9-fbb6-4061-9c25-1592ab0c2b71"
-                    target="_blank"
-                  >
-                    Privacy Policy
-                  </a>
-                </span>
-                <span className="privacy">
-                  <a
-                    href="https://www.termsandconditionsgenerator.com/live.php?token=tYqQECMeMDgxJcCqTI7Xp3663TywnoK0"
-                    target="_blank"
-                  >
-                    Terms and Conditions
-                  </a>
-                </span>
-                <span className="privacy">
-                  <a
-                    href="https://www.freeprivacypolicy.com/live/2c6571f4-af7f-4e1b-bc44-d892c53f4b46"
-                    target="_blank"
-                  >
-                    Return, Refund and Cancellation Policy
-                  </a>
-                </span>
-                <span className="privacy">
-                  <a
-                    href="https://v3056369-qgvho6pwhg5z.demo077.volusion.com/"
-                    target="_blank"
-                  >
-                    Contact us
-                  </a>
-                </span>
-                <span className="privacy">
-                  <a href="https://gi009.000webhostapp.com/" target="_blank">
-                    About us
-                  </a>
-                </span>
-              </div>
-              <div className="footer-1">
-                <p>
-                  Made with{" "}
-                  <FontAwesomeIcon icon={faHeart} style={{ color: "red" }} /> in{" "}
-                  <img src={india} width="18" alt="I Love my India" />{" "}
-                  &nbsp;&nbsp;&nbsp;All rights reserved. &copy;{" "}
-                  <a
-                    href="https://www.linkedin.com/in/kaustubh-pathak-293116198/"
-                    target="_blank"
-                    id="me"
-                  >
-                    Kaustubh Pathak
-                  </a>
+                <h5 style={{ textAlign: "center" }}>
+                  Use these Card Details ⬇️
+                </h5>
+                <p style={{ position: "absolute", marginLeft: "16%" }}>
+                  Card No : 4111 1111 1111 1111
                 </p>
+                <br />
+                <p style={{ position: "absolute", marginLeft: "16%" }}>
+                  Expiry : Any future date
+                </p>
+                <br />
+                <p style={{ position: "absolute", marginLeft: "16%" }}>
+                  Card Holder's Name : Any Name
+                </p>
+                <br />
+                <p style={{ position: "absolute", marginLeft: "16%" }}>
+                  CVV : Any number
+                </p>
+                <br />
+                <br />
+                <h5 style={{ textAlign: "center" }}>
+                  Use these UPI Details ⬇️
+                </h5>
+                <p style={{ position: "absolute", marginLeft: "16%" }}>
+                  UPI id: success@razorpay
+                </p>
+                <br />
+              </Popup>
+
+              <Navbar />
+
+              <Routes>
+                {/* This is Home Page Route */}
+                <Route path="/" element={<Home />} />
+
+                {/* This is Login Page Route */}
+                <Route path="/login" element={<Login />}></Route>
+                <Route
+                  path="/forgotpassword"
+                  element={<Forgotpassword />}
+                ></Route>
+                {/* This is Signup Page Route */}
+                <Route path="/signup" element={<Signup />}></Route>
+                <Route path="/Pricing" element={<Pricing />}></Route>
+                <Route
+                  path="/paymentsuccess"
+                  element={<Paymentsuccess />}
+                ></Route>
+                <Route path="/verify" element={<VerifyOTP />}></Route>
+                {/* This is User Page Route */}
+                <Route path="/users" element={<User />}></Route>
+                <Route path="/users/:id" element={<UserProfile />}></Route>
+                {/*This is Question Page Route*/}
+                <Route path="/Questions" element={<Questions />}></Route>
+                {/*This is Ask Question Page Route*/}
+                <Route path="/AskQuestions" element={<AskQuestion />}></Route>
+                {/*This is Ask Question Page Route*/}
+                <Route
+                  path="/Questions/:id"
+                  element={<DisplayQuestion />}
+                ></Route>
+                {/*This is Tags Page Route*/}
+                <Route path="/tags" element={<Tags />}></Route>
+              </Routes>
+
+              <Chat />
+              <div className="footer">
+                <div className="formalities">
+                  <span className="privacy">
+                    <a
+                      href="https://www.freeprivacypolicy.com/live/38db0dd9-fbb6-4061-9c25-1592ab0c2b71"
+                      target="_blank"
+                    >
+                      Privacy Policy
+                    </a>
+                  </span>
+                  <span className="privacy">
+                    <a
+                      href="https://www.termsandconditionsgenerator.com/live.php?token=tYqQECMeMDgxJcCqTI7Xp3663TywnoK0"
+                      target="_blank"
+                    >
+                      Terms and Conditions
+                    </a>
+                  </span>
+                  <span className="privacy">
+                    <a
+                      href="https://www.freeprivacypolicy.com/live/2c6571f4-af7f-4e1b-bc44-d892c53f4b46"
+                      target="_blank"
+                    >
+                      Return, Refund and Cancellation Policy
+                    </a>
+                  </span>
+                  <span className="privacy">
+                    <a
+                      href="https://v3056369-qgvho6pwhg5z.demo077.volusion.com/"
+                      target="_blank"
+                    >
+                      Contact us
+                    </a>
+                  </span>
+                  <span className="privacy">
+                    <a href="https://gi009.000webhostapp.com/" target="_blank">
+                      About us
+                    </a>
+                  </span>
+                </div>
+                <div className="footer-1">
+                  <p>
+                    Made with{" "}
+                    <FontAwesomeIcon icon={faHeart} style={{ color: "red" }} />{" "}
+                    in <img src={india} width="18" alt="I Love my India" />{" "}
+                    &nbsp;&nbsp;&nbsp;All rights reserved. &copy;{" "}
+                    <a
+                      href="https://www.linkedin.com/in/kaustubh-pathak-293116198/"
+                      target="_blank"
+                      id="me"
+                    >
+                      Kaustubh Pathak
+                    </a>
+                  </p>
+                </div>
               </div>
-            </div>
-            {
-              (window.onbeforeunload = function() {
-                localStorage.clear();
-              })
-            }
-          </BrowserRouter>
-          <ToastContainer />
+              {
+                (window.onbeforeunload = function() {
+                  localStorage.clear();
+                })
+              }
+            </BrowserRouter>
+          </div>
         </>
       )}
+      <ToastContainer />
     </div>
   );
 }
