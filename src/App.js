b2a02894e0b902a6c0f8170646dbad87b3b0fa49
Kaustubh Pathak <kaustubhpathak9@gmail.com>
2023-03-28 09:26:50 +0530

    aaaa

--- src/App.js
+++ src/App.js
@@ -96,7 +96,7 @@ function App() {
             
             <img
               src="https://storage.googleapis.com/support-forums-api/attachment/thread-54903774-5712120669965295764.jpg"
-              width="100%" height="100%"
+              width="100%" height="100vh"
             ></img>
           </div>
           <div className="portrait">
