4369dfc6657904ffeeeebc87c04135601c1f403c
Kaustubh Pathak <kaustubhpathak9@gmail.com>
2023-04-02 02:35:19 +0530

    poihg

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -397,10 +397,17 @@ const Navbar = (src) => {
                 </Avatar>
 
                 &nbsp;&nbsp;&nbsp;
-                {srcofPic && (<><img src={`${srcofPic?.src}`} alt="_" style={{
-                  borderRadius: "50%", width: "35px", height: "35px",
-                  border: "1px solid blue"
-                }}></img></>)}
+                {
+                  (srcofPic !== null) && (
+                    <img src={`${srcofPic?.src}`} alt="_" style={{
+                      borderRadius: "50%", width: "35px", height: "35px",
+                      border: "1px solid blue"
+                    }}></img>)
+                }
+
+
+
+
 
                 &nbsp;&nbsp;
                 <Link
