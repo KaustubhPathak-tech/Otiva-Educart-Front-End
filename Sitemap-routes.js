cf87df088f3eaa21a2858a3e42bbbb7de8d50547
Kaustubh Pathak <kaustubhpathak9@gmail.com>
2023-04-02 20:57:23 +0530

    seo_sitemap

---
+++ Sitemap-routes.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Route } from 'react-router';
+ 
+export default (
+    <Route>
+	<Route path='/' />
+	<Route path='/login' />
+	<Route path='/signup' />
+	<Route path='/pricing' />
+	<Route path='/paymentsuccess' />
+	<Route path='/users' />
+	<Route path='/users/:id' />
+	<Route path='/Questions' />
+	<Route path='/Questions/:id' />
+	<Route path='/AskQuestions' />
+	<Route path='/tags' />
+
+    </Route>
+);
