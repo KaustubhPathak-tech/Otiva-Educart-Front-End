17874529d9d24efa7ef423c0cfcfd512bbbab604
Kaustubh Pathak <kaustubhpathak9@gmail.com>
2023-04-03 01:07:39 +0530

    asdfghjkl

--- src/Pages/Sitemap/Sitemap.jsx
+++ src/Pages/Sitemap/Sitemap.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import axios from "axios";
+import XMLData from '../../sitemap.xml';
 const Sitemap = () => {
-
-    return (
-        <div>
-            
-        </div>
-    )
+    const [res,setRes]=useState('');
+    axios.get(XMLData, {
+        "Content-Type": "application/xml; charset=utf-8"
+     })
+     .then((response) => {
+        setRes(response.data);
+        console.log('Your xml file as string', response.data);
+     });
+  return (
+    <div>
+      {res}
+    </div>
+  )
 }
 
 export default Sitemap
