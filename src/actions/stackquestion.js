afffec3cce1f76a4473b453a09c40bcdd458996b
Kaustubh Pathak <kaustubhpathak@gmail.com>
2023-11-27 16:52:06 +0530

    removing bugs

--- src/actions/stackquestion.js
+++ src/actions/stackquestion.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 export const fetchStackquestions = () => async (dispatch) => {
     try {
         const response = await axios.get(
-          "http://localhost:7000/api/stackexchange"
+          "https://stack-overflow-server.vercel.app/api/stackexchange"
         );
         dispatch({ type: "STACK_EXCHANGE", payload: response.data.items });
       } catch (error) {
